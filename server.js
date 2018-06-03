5d5318ccbfa469ebd03b46f037c2d8299c55c494
MrSzymonello <szymonszyszko@wp.pl>
2018-06-02 17:58:32 -0700

    Modify GET /sudoku/board endpoint to support value and index query parameters

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const http = require('http');
 var app = express();
 var server = http.createServer(app);
-const { get_solved } = require('./solved-sudoku');
+const { get_solved, get_solved_with_one_element_fixed } = require('./solved-sudoku');
 const port = 8080;
 
 app.get('/sudoku/board', (req, res) => {
-    res.send(get_solved());
+    var value = req.query.value;
+    var index = req.query.index;
+    if (value !== undefined && index !== undefined) {
+        res.send(get_solved_with_one_element_fixed(value, index));
+    } else {
+        res.send(get_solved());
+    }    
 });
 
 server.listen(port, () => {
--- tests/server.test.js
+++ tests/server.test.js
@@ -14,3 +14,15 @@ describe('GET /sudoku/board', () => {
       .end(done);
   });
 });
+
+describe('GET /sudoku/board?value=3&index=12', () => {
+  it('should get 81 numbers with 12th equal to 3', (done) => {
+    request(app)
+      .get('/sudoku/board?value=3&index=12')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body[12]).to.equal(3);
+      })
+      .end(done);
+  });
+});
